3caa160a0041c16c88a1284899fdf6f49a1cf5b0
gnzlplcs <palaciosgm@outlook.com>
2023-03-17 21:59:04 -0500

    🚧 create ContactForm component + create initial template + create ContactForm component with useState

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from "react";
+import ContactForm from "./ContactForm";
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>Meteor Wallet</h1>
+    <ContactForm />
   </div>
 );
